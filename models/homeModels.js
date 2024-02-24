054ca925dcfcd755c877a22a72759b543194f075
IsaacM1210 <1210Mhamed@gmail.com>
2024-02-24 10:11:53 -0700

    init

---
+++ models/homeModels.js
@@ -0,0 +1,56 @@
+export const models = [{
+    name: "model1",
+    URL: "https://theownly.s3-ca-central-1.amazonaws.com/models/97/8ccbfbc0-020c-47b3-b387-c202fe15254e.png",
+    bed: 3,
+    bath: 2.5,
+    sqft: 2551
+},
+{
+    name: "model2",
+    URL: "https://theownly.s3-ca-central-1.amazonaws.com/models/98/df8f0a6e-10e3-4ba5-9eb2-70c3b39f48bf.png",
+    bed: 3,
+    bath: 2.5,
+    sqft: 1892
+},
+{
+    name: "model3",
+    URL: "https://theownly.s3-ca-central-1.amazonaws.com/models/541/b610e067-fc85-48b3-b2a4-0822f8c2572e.png",
+    bed: 3,
+    bath: 2.5,
+    sqft: 2149
+},
+{
+    name: "model4",
+    URL: "https://theownly.s3-ca-central-1.amazonaws.com/models/549/2b221591-b637-43cc-a1e0-485f6a6b2c4f.png",
+    bed: 3,
+    bath: 2.5,
+    sqft: 1990
+},
+{
+    name: "model5",
+    URL: "https://theownly.s3-ca-central-1.amazonaws.com/models/550/31b3ceeb-9d70-4e56-ae1e-27dcb25f3526.png",
+    bed: 3,
+    bath: 2.5,
+    sqft: 1777
+},
+{
+    name: "model6",
+    URL: "https://theownly.s3-ca-central-1.amazonaws.com/models/551/e4239c0f-b613-4fbb-9c72-84d837a8ce1e.png",
+    bed: 3,
+    bath: 2.5,
+    sqft: 1492
+},
+{
+    name: "model7",
+    URL: "https://theownly.s3-ca-central-1.amazonaws.com/models/552/25d23236-b187-48c9-b4db-c835fc44a401.png",
+    bed: 3,
+    bath: 2.5,
+    sqft: 2707
+},
+{
+    name: "model7",
+    URL: "https://theownly.s3-ca-central-1.amazonaws.com/models/822/13f67929-0148-47a3-8dca-a29234844ab9.png",
+    bed: 3,
+    bath: 2.5,
+    sqft: 2462
+}]
